88bee3ac5947159712b610207e8c0d34db4b70c7
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-06-22 03:01:50 +0700

    prepare for new UI

--- reponsive-navbar/slick.js
+++ reponsive-navbar/slick.js
@@ -8,10 +8,23 @@ $(document).ready(function () {
     pauseOnHover: true,
     cssEase: "linear",
     speed: 300,
+    fade: true,
+    draggable: false,
     dots: true,
     prevArrow:
       '<button class="PrevArrow"><i class="fa-solid fa-angle-left"></i></button>',
     nextArrow:
       '<button class="NextArrow"><i class="fa-solid fa-angle-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 987,
+        settings: {
+          draggable: true,
+          fade: false,
+          arrows: false,
+          dots: true,
+        },
+      },
+    ],
   });
 });
